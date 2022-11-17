0fd0d46a4447b36cd404d79cf5d69d98ebd8306c
MadhuniT <2019cs168@stu.ucsc.lk>
2022-11-17 12:16:49 +0530

    this commit for user request payload

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { CreateUserDto } from './userDto/createUser.dto';
+import {
+  Controller,
+  Delete,
+  Request,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Req,
+  Body,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './userDto/updateUser.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get()
-  getUser() {
-    return 'I am from user';
-  }
-
-  @Get('')
-  getAllReports() {
-    return { user: 'madhuni' };
-  }
-
-  @Get(':id')
-  getReportsById() {
-    return {};
+  getUsers() {
+    return this.userService.getUsers();
   }
 
   @Post()
-  createReport() {
-    return 'created';
+  store(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 
-  @Put(':id')
-  updateReport() {
-    return 'updated';
+  @Patch(':/userId')
+  update(
+    @Body() updateUserDto: UpdateUserDto,
+    @Param() param: { userId: number },
+  ) {
+    return this.userService.update(updateUserDto, param);
+  }
+
+  @Get('/:id')
+  getUser(@Param() param: { userId: number }) {
+    return this.userService.getAUser(param);
   }
 
   @Delete(':id')
-  deleteReport() {
-    return 'deleted';
+  deleteUser(@Param() param: { userId: number }) {
+    this.userService.deleteUser(param);
   }
 }
---
+++ src/user/userDto/updateUser.dto.ts
@@ -0,0 +1,4 @@
+export class UpdateUserDto {
+  name: String;
+  town: String;
+}
